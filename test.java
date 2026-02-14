9d3a669a2414bb92e1a9141a25a7232c7ac3824e
shreyas1si21ec088-lab <shreyas.1si21ec088@gmail.com>
2026-02-14 12:14:15 +0530

    Added Dashboard

--- test.java
+++ test.java
@@ -1,6 +1,10 @@
-import java.util.*;
-class Main {
-public static void main(String[] args){
-System.out.println("Hello World");
-}
-}
+
+async and await always inside the function
+module is new javascript after ES6
+jsx javascript xml
+react is component based programming language use of components program reusability it reduces the lines of code, improves program readability.
+React module Rules 
+1)creating While first Capital letter only
+2)In each module one main function
+3)only one return function
+4) in return only one parent div
